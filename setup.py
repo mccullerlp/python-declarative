873349865514ea8533896b169a4f1e6199358ecf
Lee McCuller <Lee.McCuller@gmail.com>
2017-01-02 18:50:22 -0500

    filling out the distribution information

---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+import sys
+from distutils.sysconfig import get_python_lib
+
+from setuptools import find_packages, setup
+
+
+version = '1.0.0a0'
+
+
+setup(
+    name='declarative',
+    version=version,
+    url='',
+    author='Lee McCuller',
+    author_email='Lee.McCuller@gmail.com',
+    description=(
+        'Collection of decorators and base classes to allow a declarative style of programming. Excellent for event-loop task registration.'
+        'Also included are nesting attribute-access dictionaries (Bunches) as well as value storage with callbacks. Relatively Magic-Free.'
+    ),
+    license='Apache v2',
+    packages=find_packages(exclude=['doc']),
+    #include_package_data=True,
+    #scripts=[''],
+    #entry_points={'console_scripts': ['',]},
+    install_requires=[],
+    extras_require={
+        "hdf" : ["h5py"],
+        "test" : ["pytest"],
+    },
+    zip_safe=False,
+    keywords = 'declarative oop bunch callback attributes metaclass',
+    classifiers=[
+        'Development Status :: 1 - Alpha ',
+        'Environment :: Libraries',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License v2',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.4',
+        #'Programming Language :: Python :: 3.5',
+        #'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: OOP Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
