d1d704f0359fd2b1e23cbdfcc818497a1b50aed4
Lee McCuller <Lee.McCuller@gmail.com>
2018-01-27 16:14:36 -0500

    added version checking to setup

--- setup.py
+++ setup.py
@@ -1,39 +1,98 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import (absolute_import, division, print_function)
+from __future__ import division, print_function, unicode_literals
 import os
 import sys
-from distutils.sysconfig import get_python_lib
-
+from os import path
 from setuptools import find_packages, setup
+from distutils.command.bdist import bdist
+from distutils.command.sdist import sdist
+
+version = '1.0.1.dev1'
+
+def check_versions():
+    print('versions checked')
+    try:
+        curpath = path.abspath(path.realpath(os.getcwd()))
+        setuppath = path.abspath(path.realpath(path.split(__file__)[0]))
+
+        if curpath != setuppath:
+            sys.path.append(setuppath)
+        import msurrogate
+
+        modfile = path.abspath(path.realpath(path.split(msurrogate.__file__)[0]))
+        mod_relpath = path.relpath(modfile, setuppath)
+        if mod_relpath == 'msurrogate':
+            if msurrogate.__version__ != version:
+                print("WARNING: Stated module version different than setup.py version", file = sys.stderr)
+                print("         '{0}' != '{1}'".format(msurrogate.__version__, version), file = sys.stderr)
+
+        import subprocess
+        try:
+            git_tag = subprocess.check_output(['git', 'describe', '--tags'])
+            git_tag = git_tag.strip()
+        except subprocess.CalledProcessError:
+            pass
+        else:
+            if git_tag != version:
+                if git_tag.startswith(version):
+                    print("WARNING: latex git-tag has commits since versioning", file = sys.stderr)
+                    print("         '{0}' ---> '{1}'".format(version, git_tag), file=sys.stderr)
+                else:
+                    print("WARNING: latex git-tag different than setup.py version", file = sys.stderr)
+                    print("         '{0}' != '{1}'".format(version, git_tag), file=sys.stderr)
+                print("         Perhaps update versions in setup.py and msurrogate.version, git commit, then git tag")
+                print("         otherwise fix tag if not yet git pushed to remote (see DISTRIBUTION-README.md)")
+
+    except ImportError:
+        pass
 
 
-version = '1.0.0.dev4'
+cmdclass = dict()
+
+class check_bdist(bdist):
+    def run(self):
+        bdist.run(self)
+        check_versions()
+
+class check_sdist(sdist):
+    def run(self):
+        sdist.run(self)
+        check_versions()
 
 
-#TODO, must warn packagers about future2.py and future3.py
+cmdclass['bdist'] = check_bdist
+cmdclass['sdist'] = check_sdist
+
+try:
+    from wheel.bdist_wheel import bdist_wheel
+    class check_bdist_wheel(bdist_wheel):
+        def run(self):
+            bdist_wheel.run(self)
+            check_versions()
+except ImportError:
+    pass
+
+cmdclass['bdist_wheel'] = check_bdist_wheel
+
 
 extra_install_requires = []
-if sys.version_info < (3,0):
+if sys.version_info < (3, 0):
     extra_install_requires.append('future')
 
 setup(
-    name='declarative',
-    version=version,
-    url='',
-    author='Lee McCuller',
-    author_email='Lee.McCuller@gmail.com',
-    description=(
+    name             = 'declarative',
+    version          = version,
+    url              = 'https://github.com/mccullerlp/python-declarative',
+    author           = 'Lee McCuller',
+    author_email     = 'Lee.McCuller@gmail.com',
+    license          = 'Apache v2',
+    packages         = find_packages(exclude = ['docs']),
+    description      = (
         'Collection of decorators and base classes to allow a declarative style of programming. Excellent for event-loop task registration.'
         'Also included are nesting attribute-access dictionaries (Bunches) as well as value storage with callbacks. Relatively Magic-Free.'
     ),
-    license='Apache v2',
-    packages=find_packages(exclude=['doc']),
-    #include_package_data=True,
-    #scripts=[''],
-    #entry_points={'console_scripts': ['',]},
-    install_requires=[
-    ] + extra_install_requires,
+    install_requires =[] + extra_install_requires,
     extras_require={
         "hdf" : ["h5py"],
         "test" : ["pytest"],
