11521c7a3b8ccd0b1aa25960a2b1c1ffe3d51c5f
Lee McCuller <Lee.McCuller@gmail.com>
2021-02-19 19:12:32 -0500

    version bump with fixes to hdf bunch and deep_bunch

--- setup.py
+++ setup.py
@@ -28,8 +28,9 @@ setup(
     install_requires =[] + extra_install_requires,
     extras_require   ={
         "hdf" : ["h5py"],
-        "test" : ["pytest"],
     },
+    tests_require=['pytest'],
+    test_suite   = 'pytest',
     cmdclass         = cmdclass,
     zip_safe         = True,
     keywords         = [
